59adc1d3419cf6bdc9f52a429f05564656d641c4
Raihan <raihan@astream.dev>
2026-05-05 09:43:57 +0800

    chore(pc): stub audio module, disable cpal dependency

--- pc/src/audio.rs
+++ pc/src/audio.rs
@@ -1,6 +1,3 @@
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use cpal::{BufferSize, SampleFormat, StreamConfig};
-
 #[derive(Debug)]
 pub struct AudioConfig {
     pub sample_rate: u32,
@@ -18,56 +15,23 @@ impl Default for AudioConfig {
     }
 }
 
-pub fn get_default_input_device() -> Option<cpal::Device> {
-    let host = cpal::default_host();
-    host.default_input_device()
+pub fn get_default_input_device() -> Option<String> {
+    Some("Default Input (Stub)".to_string())
 }
 
-pub fn get_default_output_device() -> Option<cpal::Device> {
-    let host = cpal::default_host();
-    host.default_output_device()
+pub fn get_default_output_device() -> Option<String> {
+    Some("Default Output (Stub)".to_string())
 }
 
-pub fn create_input_stream<F>(device: &cpal::Device, config: &AudioConfig, mut callback: F) -> Result<cpal::Stream, cpal::BuildStreamError>
-where
-    F: FnMut(&[f32]) + Send + 'static,
-{
-    let stream_config = StreamConfig {
-        channels: config.channels,
-        sample_rate: cpal::SampleRate(config.sample_rate),
-        buffer_size: BufferSize::Fixed(config.buffer_size),
-    };
+pub struct AudioStream;
 
-    let err_fn = |err| eprintln!("Stream error: {}", err);
-
-    device.build_input_stream(
-        &stream_config,
-        move |data: &[f32], _: &cpal::InputCallbackInfo| {
-            callback(data);
-        },
-        err_fn,
-        None,
-    )
-}
-
-pub fn create_output_stream<F>(device: &cpal::Device, config: &AudioConfig, mut callback: F) -> Result<cpal::Stream, cpal::BuildStreamError>
-where
-    F: FnMut(&mut [f32]) + Send + 'static,
-{
-    let stream_config = StreamConfig {
-        channels: config.channels,
-        sample_rate: cpal::SampleRate(config.sample_rate),
-        buffer_size: BufferSize::Fixed(config.buffer_size),
-    };
-
-    let err_fn = |err| eprintln!("Stream error: {}", err);
-
-    device.build_output_stream(
-        &stream_config,
-        move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
-            callback(data);
-        },
-        err_fn,
-        None,
-    )
+impl AudioStream {
+    pub fn new() -> Self {
+        AudioStream
+    }
+    
+    pub fn start(&self) -> Result<(), String> {
+        println!("Audio stream started (stub)");
+        Ok(())
+    }
 }
